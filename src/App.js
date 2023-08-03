444b74fb650cc4727afb2c612304961abce72554
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-03 10:42:01 +0700

    Apply Navbar Test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Time from "./components/App";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Hello!</h1>;
     </div>
   );
