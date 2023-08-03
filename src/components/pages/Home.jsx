b39c530042ff1c3e6f25d1581187a77d70da9077
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-03 17:28:03 +0700

    Home Test

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Home = () => {
     return(
-        <div className='Container'>
-            <h1>Test Page - Home</h1>
+        <div className='container'>
+            <div className="Row">
+                <div className="col-lg">
+                    <h1>Welcome to my Website!</h1>
+                    <hr/>
+                    <p  className="card-text mb-auto" style={"text-align:justify;"}>
+                   Nice to meet you, I'm TerribleSpeller. Welcome to my website! This website serves as an online portfolio and introduction to me, my projects, and my various idiosyncrasies for all those unintiated. Plus, it saves time on introductions and also allows me to get good at web developing so that I can actually properly develop my actual IRL personal site along with giving me practice in such so that I can possibly gain employment from it.</p>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
