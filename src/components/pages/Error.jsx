a074f03d7cc20f8a965d55a0d96de5c65f27e722
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-03 16:34:30 +0700

    Forgot flex-direction

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -4,7 +4,7 @@ const Error = () => {
     return(
         <div className="Container">
             <div className="Middle1Container">
-                 <div className="textContainer"  style={{ display: 'flex', alignItems: 'center', justifyContent: 'center',}}>
+                 <div className="textContainer"  style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', "flex-direction": 'column'}}>
                     <h1>Error <b>404</b> - Page Not Found</h1>
                     <img className="mb-2 mt-2" src="https://i.imgflip.com/7jff7j.jpg" alt="funni image"/>
                     <p className="mt-2;">Please return to the <a href="../index.html">Main Page</a>.</p>
