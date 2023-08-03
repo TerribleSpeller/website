41d0cdc048d956398e72f20850802ba455af9ad4
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2023-08-03 09:25:48 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
 import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <HashRouter base="/">
-    <App />
-  
-  </HashRouter>
-);
+root.render(<App />);
 
 /* Depriciated 
 ReactDOM.render(
