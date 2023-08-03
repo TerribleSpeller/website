43e31571fc98b2cfaefe00db181c41e68e583e49
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-03 16:21:36 +0700

    Fucking around and finding out with Routing

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.css"; //!!Uses !important so overrides everything.
 import "./styles.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from "./components/pages/AboutUs";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <div className="page-content">
-      <Navbar />
-      <div className="main-content">
-        <h1>Hello World!</h1>
+    <Router>
+      <div className="page-content">
+        <Navbar />
+        <div className="main-content">
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+          </Routes>        
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
+
   );
 }
 
---
+++ src/components/pages/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//Don't know why I called it AboutUs when its just me. 
+const About = () => {
+    return(
+        <div className='Container'>
+            <h1>Test Page - About Us</h1>
+        </div>
+    );
+}
+
+export default About;
