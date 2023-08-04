e42d06ecfaf48ffa96f597314725718dd5eb3b15
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2023-08-04 11:43:00 +0000

    Fixed Head

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const ContentCard = () => {
-    return(
-        <div className="d-flex rounded border card-stuff mx-auto">
-            <div className="card-stuff-top mx-auto">
-                <h3 style={{"top": "50%", "left": "50%", "text-align": "center"}}>Hello World! Test</h3>
-            </div>
-            <div className="card-stuff-bottom">
-            </div>
+  return (
+    <div className="d-flex rounded border card-stuff mx-auto">
+      <div className="card-stuff-top mx-auto ">
+        <div className="mx-3">
+          <h3 className="mt-2" style={{ top: "50%", left: "50%", "text-align": "center" }}>
+            Hello World! Test
+          </h3>
         </div>
-    );
-}
+      </div>
+      <div className="card-stuff-bottom"></div>
+    </div>
+  );
+};
 
 export default ContentCard;
--- src/components/dualCardLayout.jsx
+++ src/components/dualCardLayout.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ContentCard from "./ContentCard.jsx" 
+import React from "react";
+import ContentCard from "./ContentCard.jsx";
 const DualCardLayout = () => {
-    return(
-        <div className="col p-4 d-flex flex-row position-static">
-                <ContentCard />
-                <ContentCard />
-        </div>
-    );
-}
+  return (
+    <div className="col p-4 d-flex flex-lg-row flex-md-column position-static dualcardlayout-css">
+      <ContentCard />
+      <ContentCard />
+    </div>
+  );
+};
 
 export default DualCardLayout;
