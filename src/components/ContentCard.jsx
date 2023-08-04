e345ea496e7ae6a13dc89a71d971180e59349bb8
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2023-08-04 06:35:01 +0000

    Fixed Centring

--- src/components/ContentCard.jsx
+++ src/components/ContentCard.jsx
@@ -4,7 +4,7 @@ const ContentCard = () => {
     return(
         <div className="d-flex rounded border card-stuff mx-auto">
             <div className="card-stuff-top mx-auto">
-                <h3>Hello World! Test</h3>
+                <h3 style={{"top": "50%", "left": "50%", "text-align": "center"}}>Hello World! Test</h3>
             </div>
             <div className="card-stuff-bottom">
             </div>
