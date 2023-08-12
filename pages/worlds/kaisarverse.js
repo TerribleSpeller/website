905dfa1fe9cf0d48b1799a6fc38218bc4079c7da
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-12 18:36:15 +0700

    Missed  a Heading

--- pages/worlds/kaisarverse.js
+++ pages/worlds/kaisarverse.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import KaisarverseMain from '../../components/pages/kaisarverse-main';
+import Head from 'next/head';
+
 
 const KaisarverseContent = () => {
     return(
-        <KaisarverseMain />
+        <>
+            <Head>
+                <title>Kasiarverse - TerribleSpeller's Website</title>
+                <meta
+                 name="description"
+                 content="TerribleSpeller's Website - The Website of a Worldbuilder who can't spell well"
+                />
+            </Head>
+            <KaisarverseMain />
+        </>
+
     );
 }
 
