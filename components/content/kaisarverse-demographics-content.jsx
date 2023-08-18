d4c4beccff9e002a23a760b3a492ed76bc2a9278
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-18 10:19:50 +0700

    Bacon

--- components/content/kaisarverse-demographics-content.jsx
+++ components/content/kaisarverse-demographics-content.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from 'next/link';
+import Demgfx from '../../public/images/Demographics20.png'
+import Image from 'next/image';
 
 const KaisarverseDemographicsContent = () => {
     return(
@@ -9,10 +11,24 @@ const KaisarverseDemographicsContent = () => {
                 <div> {/*Main Content*/}
                     <table className="infobox">
                             <caption className="infobox-title ">Demographics of <span >MineLegotia and Equestria</span></caption>
-                            <tbody>
+                            <tbody>      
                                 <tr>
-                                    <td scope="row"className="infobox-left"  >Currency</td>
-                                    <td  className="infobox-right"><span className="infobox-right">Rupas (IBC, Rs) (1$ = 1.5 Rupas)</span></td>
+                                    <td colspan="2">
+                                        <span><Image src={Demgfx} alt="Demographics" width={400}/></span>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2">
+                                        <center><span>Population Pyramid, 2390 ALB</span></center>
+                                    </td>
+                                </tr>                      
+                                <tr>
+                                    <td scope="row"className="infobox-left">Population</td>
+                                    <td className="infobox-right"><span className="infobox-right">10.493.000.000 (2nd)</span></td>
+                                </tr>
+                                <tr>
+                                    <td scope="row"className="infobox-left">Growth Rate</td>
+                                    <td className="infobox-right"><span className="infobox-right">2.3% (2389 Est)</span></td>
                                 </tr>
                             </tbody>
                     </table>
