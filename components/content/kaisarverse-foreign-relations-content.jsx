fdb53967c48100892ef1a767112f092558cc54f7
TerribleSpeller <jasonalexanderyuwono@gmail.com>
2023-08-24 23:51:12 +0700

    Foreign Relations Setup

---
+++ components/content/kaisarverse-foreign-relations-content.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from 'next/link'
+import Image from 'next/image';
+
+const KaisarverseForeignRelationsContent = () => {
+    return(
+        <>
+            <h3>Nay Nay</h3>
+        </>
+    )
+}
+
+export default KaisarverseForeignRelationsContent;
