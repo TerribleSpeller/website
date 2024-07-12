354a2a0f4f08413ebe13560aa8f07390a6b45485
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-07-12 20:44:29 +0700

    Cool Little Feature + Map

--- components/js/random-subtitle.js
+++ components/js/random-subtitle.js
@@ -5,14 +5,19 @@ export default function Change() {
         "Inquisitive stepped up next to him, watching the way DIpoly's face neurtal grew",
         "Let's see, File Numbe, MO02, dash, INT985 desh, 30abZ39dee, dash RCO, dash, nintey nine", 
         "Tailspin makes sure to place a plaque next to his name plaque, which reads as BEST TREASURER FOREVER",
-        " I demand a tribute of one femboy per year",
+        "I demand a tribute of one femboy per year",
         "Good nut too vague. Make it more specific",
-        " “Dipoly,” He added, “Dipoly Diffraction of the Vigilant Judiciary.”",
-        " 'I uhh... what's PT?' Dipoly asked sheepisly.",
-        " Dipoly, the low life and very clearly corrupt Accountant",
+        "“Dipoly,” He added, “Dipoly Diffraction of the Vigilant Judiciary.”",
+        "'I uhh... what's PT?' Dipoly asked sheepisly.",
+        "Dipoly, the low life and very clearly corrupt Accountant",
         "'Professionally Guide,' Both Dipoly and Inked corrected.",
-        "'I'm competnrt,' He insisted"
-
+        "'I'm competnrt,' He insisted",
+        "Have you considered drone strikes?", 
+        "Can't we subsidise subsidising?",
+        "Webcet zo bed itch gifts achkual pwogremmehs censir",
+        "Soteria raised an eyebrow, 'A thousand lumens? That is it? I would have presumed irreality incursions would be dealt with more issues.'",
+        "Medical sighed as he shook his head, “You are incorrigible, you know?”",
+        "'You didn't... antagonise them, did you?' Dipoly asked offhoofedly.",
     ]
        
     
--- components/tempNew.js
+++ components/tempNew.js
@@ -6,7 +6,7 @@ import Change from "./js/random-subtitle.js";
 
 export default function New() {
     const NewText = Change();
-    const [buttonText, setButtonText] = useState(NewText); //same as creating your state variable where "Next" is the default value for buttonText and setButtonText is the setter function for your state variable instead of setState
+    const [buttonText, setButtonText] = useState(NewText); 
 
     const changeText = (text) => setButtonText(text);
     return (
