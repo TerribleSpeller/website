087b0c643bbf8ed6affa7cd8e766910639096ae4
TerribleSpeller <54887256+TerribleSpeller@users.noreply.github.com>
2024-07-30 16:29:14 +0700

    Map fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
-module.exports = {
+const nextConfig = {
     images: {
       domains: ['i.ibb.co'], // Use "domains" instead of "remotePatterns" if specifying only the domain
       remotePatterns: [
         {
           protocol: 'https',
-          hostname: 'i.ibb.co',
+          hostname: 'i.ibb.co.com',
           port: '',
-          pathname: '/',
+          pathname: '/gMvWWFq/**',
         },
       ],
     },
   }
+
+module.exports = nextConfig
